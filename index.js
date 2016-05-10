194d25ff33b64791bb1cbb6422104d80deeef144
yeojoey <yeojoey@gmail.com>
2016-05-10 23:29:05 +0800

    he's alive

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ var Twit = require('twit');
 
 var T = new Twit(key_obj);
 
-T.post('statuses/update', { status: 'hello?' }, function(err, data, response) {
-  console.log(data);
+var stream = T.stream('user');
+
+stream.on('tweet', function (tweet) {
+	var screenName = tweet.user.screen_name;
+	var reply = "nice meme";
+
+	if (screenName != "nice_meme_bot") {
+		var msg = "@" + screenName + " " + reply;
+
+		return T.post('statuses/update', { status: msg }, function() {
+			console.log("@" + screenName + " made a nice meme");
+		});
+	}
+
 });
